681db05dc31b907c809641a167dcec357d1bfc23
hszer0 <hszer0@gmail.com>
2015-07-30 13:35:27 +0200

    Update version number and name.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 
-setup(name="Image Checker",
-      version="0.0.1",
+setup(name="ImageChecker",
+      version="0.0.2",
       author="Patrick Liem",
       author_email="hszer0@gmail.com",
       executables=[Executable("ImageChecker.py", base=base, shortcutName="Image Checker", shortcutDir="DesktopFolder")]
